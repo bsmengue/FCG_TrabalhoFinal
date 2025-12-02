729c4c3d84e286ca65f193fb017fa2035cb499f7
Luana Hahn <88347793+luanahahn@users.noreply.github.com>
2025-12-02 00:53:58 -0300

    add collisions and tree objects

---
+++ shader_fragment.glsl
@@ -0,0 +1,79 @@
+#version 330 core
+
+// ==== OBJETO IDs (necessário no fragment shader!) ====
+#define SPHERE     0
+#define CAR        1
+#define PLANE      2
+#define CHARIZARD  3
+#define BULBASAUR  4
+#define TREE 5
+
+in vec4 position_world;
+in vec4 normal;
+in vec4 position_model;
+in vec2 texcoords;
+
+uniform mat4 view;
+
+uniform int object_id;
+uniform int texture_id;
+
+// Textures
+uniform sampler2D TextureImage0;
+uniform sampler2D TextureImage1;
+uniform sampler2D TextureImage2;
+uniform sampler2D TextureImage3;
+uniform sampler2D TextureImage4;
+uniform sampler2D TextureImage5;
+uniform sampler2D TextureImage6;
+uniform sampler2D TextureImage7;
+uniform sampler2D TextureImage8;
+
+uniform vec4 bbox_min;
+uniform vec4 bbox_max;
+
+out vec4 color;
+
+void main()
+{
+    vec4 cam_pos = inverse(view) * vec4(0,0,0,1);
+
+    vec4 p = position_world;
+    vec4 n = normalize(normal);
+
+    float U = texcoords.x;
+    float V = texcoords.y;
+
+    vec3 Kd0 = vec3(1.0);
+
+    if (object_id == PLANE)
+    {
+        if (texture_id == 0)
+            Kd0 = texture(TextureImage2, vec2(U,V)).rgb;
+
+        else if (texture_id == 1)
+            Kd0 = texture(TextureImage6, vec2(U,V)).rgb;
+    }
+    else if (object_id == SPHERE)
+    {
+        Kd0 = texture(TextureImage4, vec2(U,V)).rgb;
+        color = vec4(Kd0, 1);
+        return;
+    }
+    else if (object_id == CAR)
+        Kd0 = texture(TextureImage7, vec2(U,V)).rgb;
+
+    else if (object_id == CHARIZARD)
+        Kd0 = texture(TextureImage3, vec2(U,V)).rgb;
+
+    else if (object_id == BULBASAUR)
+        Kd0 = texture(TextureImage5, vec2(U,V)).rgb;
+
+    else if (object_id == TREE)
+        Kd0 = texture(TextureImage8, vec2(U,V)).rbg;
+
+    float lambert = max(0, dot(n, normalize(vec4(1,1,0,0))));
+    vec3 result = Kd0 * (lambert + 0.02);
+
+    color = vec4(pow(result, vec3(1.0/2.2)), 1.0);
+}
